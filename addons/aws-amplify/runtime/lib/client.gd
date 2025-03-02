77ab4126e41e302668c584b7b6cfe0358c7d0b30
lip77us <paul@lipkin.us>
2025-03-02 12:01:50 -0800

    Added Amplify
    Added Amplify Addon

---
+++ addons/aws-amplify/runtime/lib/client.gd
@@ -0,0 +1,188 @@
+class_name AWSAmplifyClient
+extends Node
+## AWS Amplify client class for handling HTTP requests.
+##
+## This class provides methods for sending HTTP requests to AWS Amplify endpoints
+## and handling responses.
+
+## Sends a GET request with JSON body.
+##
+## [param endpoint]: The URL endpoint for the request.
+## [param headers]: An array of headers to be sent with the request.
+## [param json_body]: A dictionary representing the JSON body of the request.
+## [return]: A Response object containing the result or error.
+func get_json(endpoint: String, headers: Array, json_body: Dictionary):
+	return await send_json(endpoint, headers, HTTPClient.METHOD_GET , json_body)
+
+## Sends a POST request with JSON body.
+##
+## [param endpoint]: The URL endpoint for the request.
+## [param headers]: An array of headers to be sent with the request.
+## [param json_body]: A dictionary representing the JSON body of the request.
+## [return]: A Response object containing the result or error.
+func post_json(endpoint: String, headers: Array, json_body: Dictionary):
+	return await send_json(endpoint, headers, HTTPClient.METHOD_POST, json_body)
+
+## Sends a PUT request with JSON body.
+##
+## [param endpoint]: The URL endpoint for the request.
+## [param headers]: An array of headers to be sent with the request.
+## [param json_body]: A dictionary representing the JSON body of the request.
+## [return]: A Response object containing the result or error.
+func put_json(endpoint: String, headers: Array, json_body: Dictionary):
+	return await send_json(endpoint, headers, HTTPClient.METHOD_PUT, json_body)
+
+## Sends a DELETE request with JSON body.
+##
+## [param endpoint]: The URL endpoint for the request.
+## [param headers]: An array of headers to be sent with the request.
+## [param body]: A dictionary representing the body of the request.
+## [return]: A Response object containing the result or error.
+func delete_json(endpoint: String, headers: Array, body: Dictionary):
+	return await send_json(endpoint, headers, HTTPClient.METHOD_DELETE, body)
+
+## Sends a request with JSON body using the specified HTTP method.
+##
+## [param endpoint]: The URL endpoint for the request.
+## [param headers]: An array of headers to be sent with the request.
+## [param method]: The HTTP method to use for the request.
+## [param json_body]: A dictionary representing the JSON body of the request.
+## [return]: A Response object containing the result or error.
+func send_json(endpoint: String, headers: Array, method: HTTPClient.Method, json_body: Dictionary):
+	var body = JSON.stringify(json_body)
+	if not body:
+		_error(ResponseStatus.JSON_ERROR, "request body is not json: %s" % json_body)
+	
+	var response = await send(endpoint, headers, method, body)
+	var response_value = response.result if (response.status == ResponseStatus.SUCCESS) else response.error
+	
+	var json_response_value = JSON.parse_string(response_value)
+	if !json_response_value:
+		_error(ResponseStatus.JSON_ERROR, "response result is not json: %s" % response_value)
+		
+	if response.status == ResponseStatus.SUCCESS:
+		return _success(json_response_value) 
+	else: 
+		return _error(response.status, json_response_value)
+
+## Sends a GET request.
+##
+## [param endpoint]: The URL endpoint for the request.
+## [param headers]: An array of headers to be sent with the request.
+## [param body]: The body of the request as a string.
+## [return]: A Response object containing the result or error.
+func get_(endpoint: String, headers: Array, body: String):
+	return await send(endpoint, headers, HTTPClient.METHOD_GET , body)
+
+## Sends a POST request.
+##
+## [param endpoint]: The URL endpoint for the request.
+## [param headers]: An array of headers to be sent with the request.
+## [param body]: The body of the request as a string.
+## [return]: A Response object containing the result or error.
+func post(endpoint: String, headers: Array, body: String):
+	return await send(endpoint, headers, HTTPClient.METHOD_POST, body)
+
+## Sends a PUT request.
+##
+## [param endpoint]: The URL endpoint for the request.
+## [param headers]: An array of headers to be sent with the request.
+## [param body]: The body of the request as a string.
+## [return]: A Response object containing the result or error.
+func put(endpoint: String, headers: Array, body: String):
+	return await send(endpoint, headers, HTTPClient.METHOD_PUT, body)
+
+## Sends a DELETE request.
+##
+## [param endpoint]: The URL endpoint for the request.
+## [param headers]: An array of headers to be sent with the request.
+## [param body]: The body of the request as a string.
+## [return]: A Response object containing the result or error.
+func delete(endpoint: String, headers: Array, body: String):
+	return await send(endpoint, headers, HTTPClient.METHOD_DELETE, body)
+
+## Sends an HTTP request using the specified method.
+##
+## [param endpoint]: The URL endpoint for the request.
+## [param headers]: An array of headers to be sent with the request.
+## [param method]: The HTTP method to use for the request.
+## [param body]: The body of the request as a string.
+## [return]: A Response object containing the result or error.
+func send(endpoint: String, headers: Array, method: HTTPClient.Method, body: String) -> Response:
+	var http_request = HTTPRequest.new()
+	add_child(http_request)
+
+	var error = http_request.request(endpoint, headers, method, body)
+	if error:
+		return _error(ResponseStatus.CLIENT_ERROR, "http client error: %a")
+
+	var response = await http_request.request_completed
+	http_request.queue_free()
+
+	var result = response[0]
+	var response_code = response[1]
+	var response_headers = response[2]
+	var response_body = response[3].get_string_from_utf8()
+
+	if result == HTTPRequest.RESULT_SUCCESS:
+		if response_code != 200:
+			return _error(ResponseStatus.HTTP_ERROR, response_body)
+		else:
+			return _success(response_body)
+	else:
+		return _error(ResponseStatus.REQUEST_ERROR, response_body)
+
+## Creates a success response.
+##
+## [param result]: The result to be included in the response.
+## [return]: A Response object with a success status.
+func _success(result):
+	return _response(ResponseStatus.SUCCESS, result, null)
+
+## Creates an error response.
+##
+## [param status]: The error status.
+## [param error]: The error message or object.
+## [return]: A Response object with an error status.
+func _error(status, error):
+	return _response(status, null, error)
+
+## Creates a response object.
+##
+## [param status]: The response status.
+## [param result]: The result of the request (if successful).
+## [param error]: The error message or object (if an error occurred).
+## [return]: A Response object.
+func _response(status: ResponseStatus, result, error):
+	if status == ResponseStatus.SUCCESS:
+		return Response.new(status, result, null)
+	else:
+		return Response.new(status, null, error)
+
+## Enum representing possible response statuses.
+enum ResponseStatus {
+	SUCCESS,
+	CLIENT_ERROR,
+	REQUEST_ERROR,
+	HTTP_ERROR,
+	JSON_ERROR,
+}
+
+## Class representing an HTTP response.
+class Response:
+	## The status of the response.
+	var status: ResponseStatus
+	## The result of the request (if successful).
+	var result: Variant
+	## The error message or object (if an error occurred).
+	var error: Variant
+	
+	## Initializes a new Response instance.
+	##
+	## [param _status]: The status of the response.
+	## [param _result]: The result of the request (if successful).
+	## [param _error]: The error message or object (if an error occurred).
+	func _init(_status: ResponseStatus, _result: Variant, _error: Variant):
+		status = _status
+		result = _result
+		error = _error
---
+++ amplify-godot-engine-plugin-0.3.1/README.md
@@ -0,0 +1,147 @@
+## AWS Amplify for Godot Engine - Plugin
+
+This project contains a Godot Engine plugin to interact with AWS Amplify deployed resources on AWS.
+
+## Wiki
+
+The [wiki](https://github.com/aws-samples/amplify-godot-engine/wiki) contains everything you want to know about getting started with AWS Amplify with the Godot Engine.
+
+## Install the Plugin
+
+Download the plugin
+1. Go to the [GitHub Release Section](https://github.com/aws-samples/amplify-godot-engine-plugin/releases)
+2. Click on the latest release
+3. Download the source code
+4. Extract the plugin code
+5. Copy the addons/aws-amplify folder at the root of your Godot project
+
+Enable the plugin
+1. Open Project -> Project Settings -> Plugins -> Enabled (AWS Amplify)
+2. The plugin autload the ```aws_amplify``` singleton when enabled
+
+The AWS Amplify Plugin is ready to be used!
+
+## Architecture
+
+The plugin is organized in the same way as the [amplify-js](https://github.com/aws-amplify/amplify-js) client.
+The base class AWSAmplify contains several module, each of them implementing specific features.
+You can access each module from the base class direcly with the global variable ```aws_amplify```.
+
+## Client (client)
+
+This module offers basic features to send http requests to AWS resources.
+
+You can find the source code [here](https://github.com/aws-samples/amplify-godot-engine-plugin/blob/main/addons/aws-amplify/runtime/lib/client.gd).
+
+Here's a list of all the functions with their parameters from the provided AWSAmplifyClient class:
+
+### Http Requests (TEXT)
+
+You can send http request and receive responses, with plain text bodies:
+- `get_(endpoint: String, headers: Array, body: String)`
+- `post(endpoint: String, headers: Array, body: String)`
+- `put(endpoint: String, headers: Array, body: String)`
+- `delete(endpoint: String, headers: Array, body: String)`
+- `send(endpoint: String, headers: Array, method: HTTPClient.Method, body: String)`
+
+### Http Requests (JSON)
+
+You can send http request and receive responses, with JSON bodies:
+- `get_json(endpoint: String, headers: Array, json_body: Dictionary)`
+- `post_json(endpoint: String, headers: Array, json_body: Dictionary)`
+- `put_json(endpoint: String, headers: Array, json_body: Dictionary)`
+- `delete_json(endpoint: String, headers: Array, body: Dictionary)`
+- `send_json(endpoint: String, headers: Array, method: HTTPClient.Method, json_body: Dictionary)`
+
+This module is used in other modules such as ```auth``` and ```data```.
+
+## Authentication (auth)
+
+This module offers authentication features. 
+
+You can find the source code [here](https://github.com/aws-samples/amplify-godot-engine-plugin/blob/main/addons/aws-amplify/runtime/lib/auth.gd).
+
+### Sign-Up
+- `sign_up(username, password, options: Dictionary = {})`
+- `confirm_sign_up(username: String, confirmation_code: String, options: Dictionary = {})`
+- `resend_sign_up_code(username, options: Dictionary = {})`
+
+### Sign-In
+- `sign_in(username, password, options: Dictionary = {})`
+- `reset_password(username, options: Dictionary = {})`
+- `confirm_reset_password(username, new_password, confirmation_code, options: Dictionary = {})`
+- `update_password(old_password, new_password, options: Dictionary = {})`
+
+### Sign-Out
+- `sign_out(global: bool = false)`
+- `refresh_token(refresh_token)`
+
+### User Attributes
+- `update_user_attributes(user_attributes: Dictionary, options: Dictionary = {})`
+- `update_user_attribute(user_attribute_name: String, user_attribute_value, options: Dictionary = {})`
+- `confirm_user_attribute(attribute_name: String, confirmation_code: String, options: Dictionary = {})`
+- `send_user_attribute_confirmation_code(attribute_name: String, confirmation_code: String, options: Dictionary = {})`
+- `delete_user_attributes(user_attribute_names: Array[String])`
+
+### Cached variables manipulation (tokens and user attributes)
+- `get_token(name)`
+- `get_user_attribute(name: String, refresh_attributes = false)`
+- `get_user_attributes(refresh_attributes = false)`
+- `add_user_attributes(user_attributes: Dictionary)`
+- `remove_user_attributes(keys: Array)`
+- `refresh_user(refresh_token = false, refresh_user_attributes = false)`
+
+### Authenticated http request (TEXT)
+- `get_(endpoint: String, headers: Array, body: String)`
+- `post(endpoint: String, headers: Array, body: String)`
+- `put(endpoint: String, headers: Array, body: String)`
+- `delete(endpoint: String, headers: Array, body: String)`
+- `send(endpoint: String, headers: Array, method: HTTPClient.Method, body: String)`
+
+### Authenticated http request (JSON)
+- `get_json(endpoint: String, headers: Array, json_body: Dictionary)`
+- `post_json(endpoint: String, headers: Array, json_body: Dictionary)`
+- `put_json(endpoint: String, headers: Array, json_body: Dictionary)`
+- `delete_json(endpoint: String, headers: Array, body: Dictionary)`
+- `send_json(endpoint: String, headers: Array, method: HTTPClient.Method, json_body: Dictionary)`
+
+## Data (data)
+
+This module offers GraphQL features.
+
+You can find the source code [here](https://github.com/aws-samples/amplify-godot-engine-plugin/blob/main/addons/aws-amplify/runtime/lib/data.gd)
+
+Here's a list of all the functions defined in the AWSAmplifyData class:
+
+- `query(operation, operation_name = "MyQuery", authenticated: bool = true)`
+- `mutation(operation, operation_name = "MyMutation", authenticated: bool = true)`
+- `subscription(operation, operation_name = "MySubscription", authenticated: bool = true)`
+- `send(operation, operation_name, method: GraphQLMethod, authenticated: bool = true)`
+
+## UI (AuthForm)
+
+The plugin offers an various forms to handle authentication flow. 
+
+You can find the source code [here](https://github.com/aws-samples/amplify-godot-engine-plugin/blob/main/addons/aws-amplify/runtime/ui/auth_form.gd)
+
+You can find also various samples showing how to use the form [here](https://github.com/aws-samples/amplify-godot-engine-sample/tree/feature/auth_sign_in)
+
+## Discussions
+
+If you have a question or you want to discuss with the AWS Amplify for Godot Engine community go to the main project [discussions](https://github.com/aws-samples/amplify-godot-engine/discussions) channels.
+
+## Issues
+
+If you have any issue with a custom build image [report a bug](https://github.com/aws-samples/amplify-godot-engine-plugin/issues/new?assignees=&labels=&projects=&template=bug_report.md&title=) and if you need a new image or something else  [create a feature request](https://github.com/aws-samples/amplify-godot-engine-plugin/issues/new?assignees=&labels=&projects=&template=feature_request.md&title=).
+
+## Security
+
+See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more information.
+
+## License
+
+This library is licensed under the MIT-0 License. See the [LICENSE](LICENSE.md) file.
+
+## Third Party Licenses
+
+See [THIRD_PARTY_LICENSES](THIRD_PARTY_LICENSES) for more information.
